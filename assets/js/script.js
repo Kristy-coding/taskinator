ec265b064313ccd689a3455fe187c46ae276a711
Kristy Zurmuhlen <kristyzurmuhlen@gmail.com>
2020-11-05 22:07:05 -0500

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -330,6 +330,22 @@ var saveTasks = function(){
 
 };
 
+var loadTasks = function(){
+    var savedTasks = localStorage.getItem("tasks");
+
+    if (!savedTasks) {
+    return false;
+    }
+
+    savedTasks = JSON.parse(savedTasks);
+
+    //loop through savedTasks array
+    for (var i = 0; i< savedTasks.length; i++){
+        createTaskEl(savedTasks[i]);
+    }
+
+};
+
 formEl.addEventListener("submit", taskFormHandler);
 
 pageContentEl.addEventListener("click", taskButtonHandler);
